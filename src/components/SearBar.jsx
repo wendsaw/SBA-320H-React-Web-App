bb96cc5ab81528d2cb21c728922cb0cf3abd03bf
wendsaw <sawadogo.edgard@gmail.com>
2025-03-01 12:45:27 -0700

    @latest

--- src/components/SearBar.jsx
+++ src/components/SearBar.jsx
@@ -57,7 +57,7 @@ const SearBar = () => {
                 Search
             </button>
 
-            <Bookslist books={books} />
+           {books && <Bookslist books={books} />}
         </>
     );
 
