0f00b522b4c39a6685682b3c3af8ffce61410063
wendsaw <sawadogo.edgard@gmail.com>
2025-03-02 11:05:43 -0700

    @latest

---
+++ src/layouts/RoutLayout.jsx
@@ -0,0 +1,30 @@
+
+import { NavLink, Outlet} from "react-router-dom";
+
+
+
+const RootLayout = () => {
+
+
+    return ( 
+
+        <>
+        <div className="layout">
+        <header>
+        <nav className="navbar">
+          <NavLink to='/' className="links">Home</NavLink>
+          <NavLink to='about' className="links">About</NavLink>
+          <NavLink to='help' className="links">Help</NavLink>
+        </nav>
+      </header>
+      <main>
+        <Outlet/>
+      </main>
+
+        </div>
+        
+        </>
+     );
+}
+ 
+export default RootLayout;
